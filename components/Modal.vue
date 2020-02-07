5e6e96e59b28e56a561a91b5196945727e2940d8
kfalencik <kfalencik@gmail.com>
2020-02-07 12:35:12 +0000

    broken modal on mobile

--- components/Modal.vue
+++ components/Modal.vue
@@ -18,15 +18,14 @@ export default {
   .modal {
     z-index: 9999;
     position: relative;
-    width: 100%;
+    width: 90%;
     max-width: $breakpoint-lg;
     background: $white;
-    height: calc(100vh - 200px);
+    height: calc(80vh - 50px);
     box-shadow: inset 0 0 20px $black;
     border: 10px solid #fff;
     display: flex;
     justify-content: center;
     overflow: auto;
-    padding-bottom: 50px;
   }
 </style>

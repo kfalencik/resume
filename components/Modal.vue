377139512363b025313476c42b0fa0c50919522a
kfalencik <kfalencik@gmail.com>
2020-02-07 12:32:20 +0000

    Modal width on mobile

--- components/Modal.vue
+++ components/Modal.vue
@@ -18,10 +18,10 @@ export default {
   .modal {
     z-index: 9999;
     position: relative;
-    width: 90%;
+    width: 100%;
     max-width: $breakpoint-lg;
     background: $white;
-    height: calc(80vh - 50px);
+    height: calc(100vh - 200px);
     box-shadow: inset 0 0 20px $black;
     border: 10px solid #fff;
     display: flex;
